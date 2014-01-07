00466ac8b534b28736696b3f80e088b797e28a8a
cpldcpu <cpldcpu@gmail.com>
2014-01-07 18:21:51 +0100

    usbdrv: usbconfig for attiny 10

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -39,16 +39,16 @@ these macros are defined, the boot loader uses them.
 
 /* ---------------------------- Hardware Config ---------------------------- */
 
-#define USB_CFG_IOPORTNAME      B
+//#define USB_CFG_IOPORTNAME      B
   /* This is the port where the USB bus is connected. When you configure it to
    * "B", the registers PORTB, PINB and DDRB will be used.
    */
 
-#define USB_CFG_DMINUS_BIT      3
+//#define USB_CFG_DMINUS_BIT      3
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
-#define USB_CFG_DPLUS_BIT       4
+//#define USB_CFG_DPLUS_BIT       4
 /* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
  * This may be any bit in the port, but must be configured as a pin change interrupt.
  */
@@ -63,20 +63,13 @@ these macros are defined, the boot loader uses them.
 
 /* ----------------------- Optional Hardware Config ------------------------ */
 
-/* #define USB_CFG_PULLUP_IOPORTNAME   D */
-/* If you connect the 1.5k pullup resistor from D- to a port pin instead of
- * V+, you can connect and disconnect the device from firmware by calling
- * the macros usbDeviceConnect() and usbDeviceDisconnect() (see usbdrv.h).
- * This constant defines the port on which the pullup resistor is connected.
- */
-/* #define USB_CFG_PULLUP_BIT          4 */
-/* This constant defines the bit number in USB_CFG_PULLUP_IOPORT (defined
- * above) where the 1.5k pullup resistor is connected. See description
- * above for details.
- */
-
 
 /* ------------- Set up interrupt configuration (CPU specific) -------------- */
+#ifdef __AVR_ATtiny85__
+
+#define USB_CFG_IOPORTNAME      B
+#define USB_CFG_DMINUS_BIT      3
+#define USB_CFG_DPLUS_BIT       4
 
 // setup interrupt for Pin Change for D+
 #define USB_INTR_CFG            PCMSK
@@ -87,6 +80,24 @@ these macros are defined, the boot loader uses them.
 #define USB_INTR_PENDING        GIFR
 #define USB_INTR_PENDING_BIT    PCIF
 #define USB_INTR_VECTOR         PCINT0_vect
+
+#elif __AVR_ATtiny10__
+
+#define USB_CFG_IOPORTNAME      B
+#define USB_CFG_DMINUS_BIT      1
+#define USB_CFG_DPLUS_BIT       2
+
+#define USB_INTR_CFG            PCMSK
+#define USB_INTR_CFG_SET        (1 << USB_CFG_DPLUS_BIT)
+#define USB_INTR_CFG_CLR        0
+#define USB_INTR_ENABLE         PCICR
+#define USB_INTR_ENABLE_BIT     PCIE0
+#define USB_INTR_PENDING        PCIFR
+#define USB_INTR_PENDING_BIT    PCIF0
+#define USB_INTR_VECTOR         PCINT0_vect
+
+
+#endif
 
 
 
